e6cc66af7711919db8e95cdc6fa2c2ddceb42fd3
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-24 23:35:11 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,16 @@
 # Write your solution here
-current_time = Time.now
-if current_time.now.even?
-  puts "Even!"
+current_time = Time.now.even?
+puts "Even!"
+if current_time = true
 else
   puts "Odd!"
-end
+end 
+
+
+#Assign `run_code_inside`
+#. Print out with `puts "Code before if...end"`
+#3. Reach the `if` statement
+#. Evaluate the expression to the right of the `if` statement
+# Since the expression `run_code_inside` evaluates `true`, we perform the code
+   #between the `if` and `end` reserved words
+#Print out with `puts "Code after if...end"`
